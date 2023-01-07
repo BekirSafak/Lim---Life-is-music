a9f1a9f1254aca2193f01e4456e69f894fd5bbdf
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-01-07 19:55:22 +0300

    main.js yazıldı. Hamburger menu ve active menu işlevsel halde getirildi.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('isActive');
+
+    const menuButtons = document.querySelector('.menuButtons');
+    menuButtons.classList.toggle('active')
+})
+
+
+const links = document.querySelectorAll('.links');
+
+links.forEach(buttons => {
+    buttons.addEventListener('click', function () {
+        links.forEach(btn => {
+            btn.classList.remove('activeLinks');
+            this.classList.add('activeLinks');
+        })
+    })
+})
